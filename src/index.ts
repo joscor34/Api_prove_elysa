57b7fcf6222cb747dd6027bb887febcaa70c4667
joscor34 <zbrm34@gmail.com>
2023-09-14 22:53:40 -0600

    Se modifico el index para hacer pruebas

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,28 @@
 import { Elysia } from "elysia";
+import { Context } from "hono";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+
+app.get('/ping', () => 'Pong')
+
+
+app.post('/posting', ({body, set}) => {
+  console.log(body.nombre)
+  set.status = 200  
+  return `Hola ${body.nombre}`
+
+})
+app.get('/name/:name', (context) => `Hi ${context.params.name}`)
+
+app
+  .state('version', 1 as number || null)
+  .decorate('getDate', () => Date.now)
+  .get('/version', ({
+    getDate,
+    store: { version }
+  }) => `${version} ${getDate()}`)
+
+app.listen(8080)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
